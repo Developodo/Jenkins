1c74f40805fdb67a1e668d07f220ded2cd2540a0
Your Name <you@example.com>
2024-06-23 19:47:58 +0200

    Jenkindfile ready

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env sh
 
+# Navegar a la carpeta del proyecto si es necesario
+cd /ruta/al/proyecto
+
 # Construir la aplicación
+npm install
 npm run build
 
-# Instalar pm2 si no está instalado (esto se puede omitir si ya está instalado globalmente)
-npm install -g pm2
+# Iniciar la aplicación en segundo plano
+npm start > app.log 2>&1 &
 
-# Iniciar la aplicación usando pm2
-pm2 start npm --name "jenkins-app" -- start
+# Guardar el PID del proceso
+echo $! > .pidfile
+
+# Darle tiempo para que se inicie
+sleep 5
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
