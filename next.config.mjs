84e3f205e57923f46aa1c75a64c42605f8c51040
root <you@example.com>
2024-06-24 09:08:37 +0000

    prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix : './'	
+    assetPrefix : 'http://localhost/app'	
 };
 
 export default nextConfig;
